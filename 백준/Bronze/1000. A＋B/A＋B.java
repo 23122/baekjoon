a8fbcb436b1c4fc21c71ba97a660de4f1e13c2b0
ParkJongJin <100248506+23122@users.noreply.github.com>
2024-02-18 16:41:01 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17680 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Main{
     public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int a= sc.nextInt();
         int b= sc.nextInt();
         if(a>0 || b<10){
