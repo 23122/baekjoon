9b9f686f1120f7665521d6e37e7bb4f496e4c3dc
ParkJongJin <100248506+23122@users.noreply.github.com>
2024-02-25 17:54:49 +0900

    [Silver V] Title: 소트인사이드, Time: 120 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,28 +1,18 @@
 import java.io.*;
 import java.util.Arrays;
-import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String target = br.readLine();
-        Integer[] result = new Integer[target.length()];
+        char[] target = br.readLine().toCharArray();
 
-        for (int i = 0; i < target.length(); i++) {
-            result[i] = Character.getNumericValue(target.charAt(i));
+        Arrays.sort(target);
+
+        for(int i = target.length-1; i >= 0 ; i--){
+            bw.write(target[i]);
         }
-
-        Arrays.sort(result, Collections.reverseOrder());
-
-        Arrays.stream(result).forEach(num -> {
-            try {
-                bw.write(String.valueOf(num));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
 
         bw.flush();
         bw.close();
