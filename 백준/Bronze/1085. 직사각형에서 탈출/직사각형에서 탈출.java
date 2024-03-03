53feb23a2d0be7e7792c2c024b19269d7ed4a6d3
ParkJongJin <100248506+23122@users.noreply.github.com>
2024-03-03 21:33:25 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 124 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] list = br.readLine().split(" ");
+
+        int[] min = new int[2];
+
+        for(int i = 0 ; i < list.length / 2 ; i++){
+            int n = Integer.parseInt(list[i]);
+            int m = Integer.parseInt(list[i+2]);
+
+            min[i % 2] = Math.min(n,m-n);
+        }
+
+        int result = Math.min(min[0],min[1]);
+
+        bw.write(String.valueOf(result));
+
+        bw.flush();
+        bw.close();
+    }
+}
