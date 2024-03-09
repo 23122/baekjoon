ff1337703bbc2e1dcdc99e9da9c0b3b53c9c6eac
ParkJongJin <100248506+23122@users.noreply.github.com>
2024-03-09 22:00:30 +0900

    [Silver V] Title: 셀프 넘버, Time: 136 ms, Memory: 14728 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -1,25 +1,27 @@
 import java.io.*;
-import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int[] i      = new int[10001];
-        int[] number = new int[10001];
+        boolean[] check = new boolean[10001];
 
-        for(int j = 0 ; j <= i.length-1 ; j++){
-            number[j] = j;
-            char[] list = String.valueOf(j).toCharArray();
-            i[j] = j;
+        for(int i = 0 ; i < check.length ; i++){
+            char[] list = String.valueOf(i).toCharArray();
+            int num = i;
+
             for(char c : list){
-                i[j] += Character.getNumericValue(c);
+                num += Character.getNumericValue(c);
+            }
+
+            if(num < check.length){
+                check[num] = true;
             }
         }
 
-        for(int target : number){
-            if(IntStream.of(i).noneMatch(result -> result == target)){
-                bw.write(String.valueOf(target));
+        for(int j = 0 ; j < check.length ; j++){
+            if(!check[j]){
+                bw.write(String.valueOf(j));
                 bw.newLine();
             }
         }
