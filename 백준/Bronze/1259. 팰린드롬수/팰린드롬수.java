b6b5d5774496c2d3ea9e1260d48029906446c975
ParkJongJin <100248506+23122@users.noreply.github.com>
2024-03-17 22:55:12 +0900

    [Bronze I] Title: 팰린드롬수, Time: 116 ms, Memory: 14056 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String result = "yes";
+
+        while (true){
+            char[] line = br.readLine().toCharArray();
+            if(line[0] == '0'){
+                break;
+            }
+            for(int i = 0 ; i <= line.length / 2 ; i++){
+                if(line[i] != line[line.length - 1 - i]){
+                    result = "no";
+                }
+            }
+            bw.write(result);
+            bw.newLine();
+            result = "yes";
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
