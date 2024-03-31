e7443e22d23d2c61e484d2d25a3f5448b3fc2abc
ParkJongJin <100248506+23122@users.noreply.github.com>
2024-03-31 21:02:54 +0900

    [Silver IV] Title: 덱, Time: 252 ms, Memory: 20256 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+
+            switch (command) {
+                case "push_front":
+                    int numFront = Integer.parseInt(st.nextToken());
+                    deque.offerFirst(numFront);
+                    break;
+                case "push_back":
+                    int numBack = Integer.parseInt(st.nextToken());
+                    deque.offerLast(numBack);
+                    break;
+                case "pop_front":
+                    if (deque.isEmpty()) {
+                        bw.write("-1\n");
+                    } else {
+                        bw.write(deque.pollFirst() + "\n");
+                    }
+                    break;
+                case "pop_back":
+                    if (deque.isEmpty()) {
+                        bw.write("-1\n");
+                    } else {
+                        bw.write(deque.pollLast() + "\n");
+                    }
+                    break;
+                case "size":
+                    bw.write(deque.size() + "\n");
+                    break;
+                case "empty":
+                    bw.write((deque.isEmpty() ? 1 : 0) + "\n");
+                    break;
+                case "front":
+                    if (deque.isEmpty()) {
+                        bw.write("-1\n");
+                    } else {
+                        bw.write(deque.peekFirst() + "\n");
+                    }
+                    break;
+                case "back":
+                    if (deque.isEmpty()) {
+                        bw.write("-1\n");
+                    } else {
+                        bw.write(deque.peekLast() + "\n");
+                    }
+                    break;
+                default: break;
+            }
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+}
