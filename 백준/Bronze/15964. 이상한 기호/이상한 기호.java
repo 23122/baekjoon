fe032b67d127e62af33ee48416afd0d1919d24cd
ParkJongJin <100248506+23122@users.noreply.github.com>
2024-04-09 23:26:05 +0900

    [Bronze V] Title: 이상한 기호, Time: 128 ms, Memory: 14552 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -1,16 +1,17 @@
 import java.io.*;
+import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader  br    = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter  bw    = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] line = br.readLine().split(" ");
-        int      a    = Integer.parseInt(line[0]);
-        int      b    = Integer.parseInt(line[1]);
+        String[]   line = br.readLine().split(" ");
+        BigInteger a    = BigInteger.valueOf(Long.parseLong(line[0]));
+        BigInteger b    = BigInteger.valueOf(Long.parseLong(line[1]));
 
-        bw.write(String.valueOf((a + b) * (a - b)));
-        
+        bw.write(String.valueOf(a.pow(2).subtract(b.pow(2))));
+
         br.close();
         bw.flush();
         bw.close();
